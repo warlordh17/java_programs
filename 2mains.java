bdaed53aa20805cc59d8dc3d72220af88299dd60
warlordh17 <72943207+warlordh17@users.noreply.github.com>
2020-10-18 13:22:31 +0530

    Add files via upload
    2nd commit

---
+++ 2mains.java
@@ -0,0 +1,16 @@
+class Twomains
+{
+  public static void main(String [] boy)
+    {
+           String s = "sayam is fantastic!!!!";
+            print(s);
+    }
+public static void print(String s)
+       {
+	System.out.println(s);
+		System.out.println(s);
+			System.out.println(s);
+				System.out.println(s);
+					System.out.println(s);
+       }
+}
