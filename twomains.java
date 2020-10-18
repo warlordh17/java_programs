cfba7047b17ede391b0c62b44ed448797f2616c7
warlordh17 <72943207+warlordh17@users.noreply.github.com>
2020-10-18 13:21:35 +0530

    Add files via upload
    2nd commit

---
+++ twomains.java
@@ -0,0 +1,12 @@
+class Twomains
+{
+     public static void main(String [] boys)
+	{
+		  System.out.println("very good");
+	}
+	 public static void main(String [] girls)
+	{
+		  System.out.println("worst");
+	}
+}
+main();
